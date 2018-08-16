8a18d2f6dc7434ac8081c25fb0ebc9f95c4b6fc1
Anthamation <Anthamation@gmail.com>
2018-08-15 20:54:33 -0500

    Stable-Version-1.0.5

--- AnthaBot.js
+++ AnthaBot.js
@@ -34,6 +34,7 @@ client.on('message', msg => {
     if (msg.content.includes("https://discord.gg/")){
         msg.delete()
         msg.member.send('You are reminded that outside Discord server links are not permitted in the server.')
+        var GuildID = client.guilds.get('404304756845051905')
         GuildID.fetchMember(msg.author.id).then (member => {
         console.log(`${member.displayName} has had it's Discord link obliterated on ${CurrentTime}`)
         })}
